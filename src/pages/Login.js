3c38f14071d0a9ff7381a60231af28eac26c0a0e
antanim <abdurnouutanim11@gmail.com>
2023-04-10 02:36:26 +0600

    delete logo from signup and login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
-import Logo from "../components/Logo";
+import Navbar from "../components/Navbar";
 
 const Login = () => {
   return (
-    <section className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex justify-center">
-      <div className="max-w-screen-xl m-0 sm:m-10 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-center flex-1">
-        {/* form content  */}
-        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          {/* logo  */}
-          <Link to="/">
-            <Logo className="hover:cursor-pointer" />
-          </Link>
-          {/* form  */}
-          <LoginForm />
+    <>
+      <Navbar />
+      <section className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex justify-center">
+        <div className="max-w-screen-xl m-0 mt-16 md:mt-20 sm:m-10 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-center flex-1">
+          {/* form content  */}
+          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+            {/* form  */}
+            <LoginForm />
+          </div>
+          {/* illustration  */}
+          <div className="flex-1 bg-indigo-100 dark:bg-indigo-950 text-center hidden lg:flex">
+            <div
+              className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+              style={{
+                backgroundImage: `url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg`,
+              }}
+            ></div>
+          </div>
         </div>
-        {/* illustration  */}
-        <div className="flex-1 bg-indigo-100 dark:bg-indigo-950 text-center hidden lg:flex">
-          <div
-            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-            style={{
-              backgroundImage: `url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg`,
-            }}
-          ></div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../components/Logo";
+import Navbar from "../components/Navbar";
 import SignupForm from "../components/SignupForm";
 
 const Signup = () => {
   return (
-    <section className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex justify-center">
-      <div className="max-w-screen-xl m-0 sm:m-10 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-center flex-1">
-        {/* form content  */}
-        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          {/* logo  */}
-          <Link to="/">
-            <Logo className="hover:cursor-pointer" />
-          </Link>
-          {/* form  */}
-          <SignupForm />
+    <>
+      <Navbar />
+      <section className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 flex justify-center">
+        <div className="max-w-screen-xl m-0 mt-14  sm:m-20 bg-white dark:bg-gray-800 shadow sm:rounded-lg flex justify-center flex-1">
+          {/* form content  */}
+          <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+            <SignupForm />
+          </div>
+          {/* illustration  */}
+          <div className="flex-1 bg-indigo-100 dark:bg-indigo-950 text-center hidden lg:flex">
+            <div
+              className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+              style={{
+                backgroundImage: `url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg`,
+              }}
+            ></div>
+          </div>
         </div>
-        {/* illustration  */}
-        <div className="flex-1 bg-indigo-100 dark:bg-indigo-950 text-center hidden lg:flex">
-          <div
-            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-            style={{
-              backgroundImage: `url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg`,
-            }}
-          ></div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
